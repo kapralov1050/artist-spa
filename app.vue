8ae04f3823321ac9d72aa08e4252c74b72f21726
Alexander Kapralov <pereulkovakapralov@gmail.com>
2025-06-06 19:38:33 +0300

    fix: fix vercel deploy error

--- app.vue
+++ app.vue
@@ -34,20 +34,20 @@
       </article>
       <article class="gallery">
         <div class="gallery__content">
-          <div class="item"><img src="/gallery/2.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/3.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/4.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/5.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/6.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/7.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/8.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/9.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/10.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/11.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/12.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/13.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/14.jpg" alt="alt"></div>
-          <div class="item"><img src="/gallery/15.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/2.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/3.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/4.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/5.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/6.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/7.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/8.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/9.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/10.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/11.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/12.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/13.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/14.jpg" alt="alt"></div>
+          <div class="item"><img src="public/gallery/15.jpg" alt="alt"></div>
         </div>
       </article>
     </div>
